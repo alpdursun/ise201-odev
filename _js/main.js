fd4bc195a9a80dc4300c224d4c111a1fdc8f46b2
alpdursun <alp.dursun@ogr.sakarya.edu.tr>
2019-12-21 21:15:17 +0300

    Index sayfası düzenlendi

---
+++ _js/main.js
@@ -0,0 +1,9 @@
+/* Menü Javascript Kodu */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
