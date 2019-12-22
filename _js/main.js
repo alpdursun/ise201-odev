eb3ad6a213fbadd3b78a1a0bcfa9fde60fb8a5e8
alpdursun <alp.dursun@ogr.sakarya.edu.tr>
2019-12-22 17:13:22 +0300

    İletişim Formu Kontrolü Javascript Kodu Eklendi

--- _js/main.js
+++ _js/main.js
@@ -1,4 +1,5 @@
 /* Menü Javascript Kodu */
+
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,3 +8,37 @@ function myFunction() {
     x.className = "topnav";
   }
 } 
+
+
+/* Form Kontrolü İçin Gerekli Javascript Kodları */
+
+function validateForm() {
+  var x = document.forms["myForm"]["isim"].value;
+  var y = document.forms["myForm"]["email"].value;
+  var z = document.forms["myForm"]["mesaj"].value;
+
+  var gecerli = 1;
+
+  var regex = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+.)+([.])+[a-zA-Z0-9.-]{2,4}$/;
+	
+
+  if (x == "") {
+    alert("Lütfen İsim Bölmesini Doldurunuz");
+    gecerli = 0;
+  }
+
+  if (regex.test(y)!=true) {
+    alert("Lütfen Geçerli Bir E-Mail Adresi Giriniz");
+    gecerli = 0;
+  }
+
+  if(z == " ") {
+    alert("Lütfen Mesaj Bölmesini Doldurunuz");
+    gecerli = 0;
+  }
+
+  if(gecerli==0){
+    gecerli = 1;
+    return false;
+  }
+}
